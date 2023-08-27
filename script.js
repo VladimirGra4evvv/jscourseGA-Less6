d7f2a6c93f47b029615f3fdc8fd9aa335c4da8a5
Vladimir Gra4ev <gra4evvv@yandex.ru>
2023-08-27 18:55:24 +0300

    поправлено и выполнено ДЗ 6

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//Написать игровой бот.
+// Для этого задания создайте отдельный репозиторий.
+// Используйте функции alert, confirm, prompt для общения с пользователем. 
+
+// Написать игровой бот.
 //"Загадывание случайного числа от 1 до 100"
 //Что должна делать программа:
 //— спрашивает пользователя: "Угадай число от 1 до 100".
@@ -12,9 +15,7 @@
 
 'use strict';
 
-
-//==================================== функции =============================================
-const isNumber = function (n) {
+const isNumber = (n) => {
     let tempVar =   !isNaN(parseFloat(n)) 
                     && isFinite(n)
                     && +n >= 1 
@@ -24,77 +25,34 @@ const isNumber = function (n) {
         return tempVar;
     };
 
-//Генерирует случайное целое число от min до max
-const getRandomInteger = function (min, max) {
-    // получить случайное число от (min-0.5) до (max+0.5)
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
+const getNumber = (title, defaultValue = '') => {
+    let tempVariable;
+    do {
+        tempVariable = prompt(title, defaultValue);
+    } while (!isNumber(tempVariable));
+    return +tempVariable;
+};
+
+const game = () => {
+
+    const randomNumber = Math.floor(Math.random() * 100);
+
+    const asking  = () => {
+        const hiddenNumber = getNumber('Введите число от 1 до 100');
+
+        if (hiddenNumber > randomNumber) {
+            alert("Загаданное число меньше " + hiddenNumber);
+            asking()} 
+        else if (hiddenNumber < randomNumber) {
+            alert("Загаданное число больше " + hiddenNumber);
+            asking()} 
+        else if (hiddenNumber === randomNumber) {
+            alert("Число угадано! Это было число " + randomNumber);
+            alert("Игра окончена!")
+        };
     };
 
-// Принимает только целое число от 1 до 100 и отдает в виде Number
-const getNumber = function (title, defaultValue = '') {
-        let tempVariable;
-        do {
-            tempVariable = prompt(title, defaultValue);
-        } while (!isNumber(tempVariable));
-        return +tempVariable;
-    };
-    
-// ============================== организация замыкания =====================================
-function createGame () {
-    // генерирование числа от 1 до 100
-    const n = getRandomInteger(1, 100);
-    // счетчик попыток
-    let count = 10;
-// возвращает объект с ключами result и count
-    return function (num) {
-        let result;
-        count--;
-        if (num === n) {
-            result = 'bingo';
-        } else if (num > n) {
-            result = 'big';
-        } else {
-            result = 'small';
-        }
-        return {
-            'result': result,
-            'count': count,};
-    };
-}
+    asking();
+};
 
-function getStartNewGame (start) {
-
-    if (!start) {return;}
-        
-    const game = createGame();
-    let num = getNumber('Угадай целое число от 1 до 100');
-
-    function gameBody(callback) {
-        let answer = callback(num);
-
-        if (answer.count < 1) {
-            let startNewGame = confirm('Попытки закончились, хотите сыграть еще?');
-            if (!startNewGame) {alert('До свидания!');}
-            getStartNewGame(startNewGame);
-
-        } else if (answer.result === 'big') {
-            num = getNumber('Загаданное число меньше, осталось попыток: ' + answer.count);
-            gameBody(callback);
-
-        } else if (answer.result === 'small') {
-            num = getNumber('Загаданное число больше, осталось попыток: ' + answer.count);
-            gameBody(callback);
-
-        } else if (answer.result === 'bingo') {
-            let startNewGame = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-            if (!startNewGame) {alert('До свидания!');}
-            getStartNewGame(startNewGame);
-        }
-    }
-
-    gameBody(game);
-}
-
-getStartNewGame(true);
-console.log('Игра окончена');
+game ();
